29efac798f2737e48f8e161b9f5b12c295175125
Kim Eik <kim@heldig.org>
2020-10-01 22:25:14 +0200

    added some additional options to terminal printout

--- main.go
+++ main.go
@@ -7,15 +7,23 @@ import (
 	"io"
 	"log"
 	"os"
+	"time"
 )
-var input = flag.String("input", "-", "input source to use")
+var input = flag.String("input", "", "input source to use or '-' for stdin")
 var iteration = flag.Int("generations",1, "number of generations to increment")
+var pdelay = flag.Int("print-delay",0, "delay in ms between print of generations")
+var clear = flag.Bool("clear",false, "clear terminal between printout (ansi only)")
 
 func main() {
 	var err error
 	var reader io.Reader = os.Stdin
 
 	flag.Parse()
+
+	if *input == ""{
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
 
 	if *input != "-"{
 		fh,err := os.Open(*input)
@@ -32,7 +40,13 @@ func main() {
 	}
 
 	for ; *iteration > 0; *iteration-- {
+		if *clear {
+			//fmt.Print("\033[2J") //clear ansi terminal
+			//fmt.Print("\033[H") //put cursor at beginning
+			fmt.Print("\033c") //reset terminal
+		}
 		g = g.NextGeneration()
 		fmt.Println(g.String())
+		time.Sleep(time.Millisecond*time.Duration(*pdelay))
 	}
 }
